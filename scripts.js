170d890e01b64ac592c4f1ebb3e62a07c0dbd60a
pradip2004 <pradipsadhukhan16@gmail.com>
2024-06-15 22:10:59 +0530

    fixed product section, add about honey section

---
+++ scripts.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  freeMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  // mousewheel: true,
+  keyboard: true,
+});
