5de357008ee8f2273e8e64a214c8db02b00ec089
pradip2004 <pradipsadhukhan16@gmail.com>
2024-06-22 23:07:08 +0530

    fixing mobile devices

--- scripts.js
+++ scripts.js
@@ -8,6 +8,10 @@ var swiper = new Swiper(".mySwiper", {
   },
   keyboard: true,
   breakpoints: {
+    325: {
+      slidesPerView: 2,  // Adjust slides per view for devices with a width of 475px and above
+      spaceBetween: 30,
+    },
     768: {
       slidesPerView: 3,  // Adjust slides per view for devices with a width of 768px and above
       spaceBetween: 20,  
