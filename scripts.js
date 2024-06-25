d86a4f6186fd8c3fd22f4f7dfe9ddf271285e364
pradip2004 <pradipsadhukhan16@gmail.com>
2024-06-25 21:58:02 +0530

    add product page animation

--- scripts.js
+++ scripts.js
@@ -9,84 +9,110 @@ var swiper = new Swiper(".mySwiper", {
   keyboard: true,
   breakpoints: {
     325: {
-      slidesPerView: 2,  
+      slidesPerView: 2,
       spaceBetween: 30,
     },
     768: {
-      slidesPerView: 3,  
-      spaceBetween: 20,  
+      slidesPerView: 3,
+      spaceBetween: 20,
     },
     1024: {
-      slidesPerView: 4,  
-      spaceBetween: 25,  
+      slidesPerView: 4,
+      spaceBetween: 25,
     }
-}});
+  }
+});
 
 
-
-let menu = document.querySelector(".nav_menu img");
-let extented_nav = document.querySelector(".extented_nav");
-let close = document.querySelector(".close");
-console.log(menu);
-menu.addEventListener("click", ()=>{
-  // console.log("hello");
+function navbarAnimation() {
+  let menu = document.querySelector(".nav_menu img");
+  let extented_nav = document.querySelector(".extented_nav");
+  let close = document.querySelector(".close");
+  console.log(menu);
+  menu.addEventListener("click", () => {
+    // console.log("hello");
     let etl = gsap.timeline()
-    etl.to(extented_nav,{
-        top: 0,
-        duration: 1,
-        ease: "expo.out"
+    etl.to(extented_nav, {
+      top: 0,
+      duration: 1,
+      ease: "expo.out"
     })
     etl.from(".extented_nav_part1", {
       x: -40,
       duration: 1,
       stagger: 0.1,
       opacity: 0
-  }, 'a');
-  
-  etl.from(".extented_nav_part2", {
+    }, 'a');
+
+    etl.from(".extented_nav_part2", {
       x: 40,
       duration: 1,
       stagger: 0.1,
       opacity: 0
-  }, 'a');
-})
-close.addEventListener('click', ()=>{
+    }, 'a');
+  })
+  close.addEventListener('click', () => {
     gsap.to(extented_nav, {
-        top: -500,
-        duration: 1,
-        ease: "expo.out"
+      top: -500,
+      duration: 1,
+      ease: "expo.out"
     })
-})
+  })
+
+}
+
+function homePageAnimation() {
+  gsap.from(".navbar", {
+    y: -100,
+    duration: 1.5,
+    opacity: 0
+  })
+
+  gsap.from(".hero_heading", {
+    x: -100,
+    duration: 1.5,
+    opacity: 0
+  })
+
+  gsap.from(".right_container .hero_img", {
+    x: 100,
+    duration: 1.5,
+    opacity: 0
+  })
+
+  gsap.to(".bee_img img", {
+    duration: 3,
+    opacity: 1,
+    motionPath: {
+      path: "#beePath",
+      align: "#beePath",
+      alignOrigin: [0.5, 0.5],
+      autoRotate: true,
+      start: 0,
+      end: 0.85
+    }
+  })
+
+}
 
 
-gsap.from(".navbar", {
-  y: -100,
-  duration: 1.5,
-  opacity: 0
-})
+const trace_honey_btn = document.querySelector(".trace_honey_btn");
+const product_popup = document.querySelector(".product_popup_section");
+const popup_close_btn = document.querySelector(".product_popup_section_close");
+trace_honey_btn.addEventListener("click", () => {
+  gsap.to(product_popup, {
+    duration: 0.5,
+    opacity: 1,
+    scale: 1,
+    visibility: "visible"
+  });
+});
 
-gsap.from(".hero_heading", {
-  x: -100,
-  duration: 1.5,
-  opacity: 0
-})
-
-gsap.from(".right_container .hero_img", {
-  x: 100,
-  duration: 1.5,
-  opacity: 0
-})
-
-gsap.to(".bee_img img", {
-  duration: 3,
-  opacity: 1,
-  motionPath: {
-    path: "#beePath",
-    align: "#beePath",
-    alignOrigin: [0.5, 0.5],
-    autoRotate: true,
-    start: 0,
-    end: 0.85
-  }
-})
-
+popup_close_btn.addEventListener("click", () => {
+  gsap.to(product_popup, {
+    duration: 0.5,
+    scale: 0,
+    opacity: 0,
+    visibility: "hidden"
+  });
+});
